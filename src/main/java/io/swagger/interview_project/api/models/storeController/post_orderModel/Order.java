e936c9d63532c4abefa9d13c49bb1a2a2454fdd7
denys.kozachok <denys.kozachok@imperva.com>
2024-10-28 22:51:20 -0700

    Remove useless JsonProperty

--- src/main/java/io/swagger/interview_project/api/models/storeController/post_orderModel/Order.java
+++ src/main/java/io/swagger/interview_project/api/models/storeController/post_orderModel/Order.java
@@ -1,7 +1,6 @@
 package io.swagger.interview_project.api.models.storeController.post_orderModel;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.ToString;
 import lombok.experimental.Accessors;
@@ -12,21 +11,10 @@ import lombok.experimental.Accessors;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class Order{
 
-	@JsonProperty("petId")
 	private Integer petId;
-
-	@JsonProperty("quantity")
 	private Integer quantity;
-
-	@JsonProperty("id")
 	private Integer id;
-
-	@JsonProperty("shipDate")
 	private String shipDate;
-
-	@JsonProperty("complete")
 	private Boolean complete;
-
-	@JsonProperty("status")
 	private String status;
 }
